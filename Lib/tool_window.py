c176011f5104587f50a1c7f61d75bdafa578a823
adventurer255 <1611802396@qq.com>
2024-04-28 13:44:53 +0800

    hint mainwindow

--- Lib/tool_window.py
+++ Lib/tool_window.py
@@ -1,11 +1,12 @@
 from .ui_tool_window import Ui_Tool_Window
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets,QtCore
 
 
 class ToolWindow(Ui_Tool_Window, QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
 
 
 if __name__ == '__main__':
